3dee3e69fc0c7007e187e2de25e5cf8e1bd706bf
iqbaal <muhikbal0999@gmail.com>
2025-12-09 21:06:16 +0800

    terbaru

--- Analisis.py
+++ Analisis.py
@@ -7,10 +7,7 @@ from sklearn.naive_bayes import ComplementNB
 from sklearn.linear_model import LogisticRegression # Model Kedua
 from sklearn.metrics import classification_report, accuracy_score
 from imblearn.over_sampling import SMOTE # Teknik Over-sampling
-# "ini saya punya"
-#HFUGFIUFUOIICNDICDI RENDI
-# REEEE
-# dani punya
+
 # =========================================================
 # ⚠️ GANTI DENGAN NAMA 3 FILE CSV ANDA! ⚠️
 FILE_NAMES = [
